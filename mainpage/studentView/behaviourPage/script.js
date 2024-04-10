0d9ce0b24a07601e6772a1beffe71e9785228a95
iamagoodacc <wangzihan321@gmail.com>
2024-04-11 00:03:05 +0100

    addition of new behaviour events and scrolling frame

--- mainpage/studentView/behaviourPage/script.js
+++ mainpage/studentView/behaviourPage/script.js
@@ -1,17 +1,25 @@
+var behaviourEventTemplate = document.getElementById("behaviourEventTemplate")
+var recentBehaviourEvents = behaviourEventTemplate.parentElement
+for (i=0; i < 15; i++) {
+    var newBehaviourEventContents = document.importNode(behaviourEventTemplate.content, true);
+    var newBehaviourEvent = document.createElement("div")
+    newBehaviourEvent.appendChild(newBehaviourEventContents)
+    newBehaviourEvent.classList.add("behaviourEvent")
+    recentBehaviourEvents.appendChild(newBehaviourEvent);
+}
+
 document.querySelectorAll(".dropDownIcon").forEach(function(viewMoreToggle) {
     $(viewMoreToggle).click(function() {   
         var description = viewMoreToggle.parentElement 
         var expanded = description.querySelector(".text") == null;
 
         if (!expanded) {
-            viewMoreToggle.classList.add("expand")
-            viewMoreToggle.classList.remove("expand")
+            viewMoreToggle.style.transform = "rotate(180deg)";
             var textBox = description.querySelector(".text")
             textBox.classList.remove("text")
             textBox.classList.add("expandedText")
         } else if (expanded) {
-            viewMoreToggle.classList.add("expand")
-            viewMoreToggle.classList.remove("expand")
+            viewMoreToggle.style.transform = "rotate(0deg)";
             var textBox = description.querySelector(".expandedText")
             textBox.classList.remove("expandedText")
             textBox.classList.add("text")
